4a360f079ee0930909548a23b207643fb16eb68f
Duncan <MacKinnon>
2024-03-30 23:01:43 +0000

    Added Props to the highlight section

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Highlight from "./Components/Highlights";
 import Footer from "./Components/Footer";
 
+import Topiary from './assets/Gardening-Services/Topiary.jpg';
+
 function App() {
   return (   
     <div>
-      <Highlight />
+      <Highlight title="First Highlight" description="Experience our beautiful botanic gardens and the most biodiverse place on Earth." imagePath={Topiary} imageDescription="Topiary"/>
+      <Highlight title="Second Highlight" description="Experience our beautiful botanic gardens and the most biodiverse place on Earth." imagePath={Topiary} imageDescription="Topiary"/>
       <Footer />
 
     </div>
--- react-application/src/Components/Highlights.jsx
+++ react-application/src/Components/Highlights.jsx
@@ -1,27 +1,16 @@
 import './Styles/Highlight-Styles.css';
-import Topiary from '../assets/Gardening-Services/Topiary.jpg';
 
-function Highlights(){
+function Highlights(props){
     return(
         <div className="highlights-page">
             <div className="highlights-section">
                 <div className="highlights-content">
                     <div className="highlights-image-wrapper">
-                        <img className="highlights-image" src={Topiary} alt="Meaningful Text"/>
+                        <img className="highlights-image" src={props.imagePath} alt={props.imageDescription}/>
                     </div>
                     <div className="highlights-text">
-                        <h3>Visit the world-famous Kew Gardens</h3>  
-                        <p>Experience our beautiful botanic gardens and the most biodiverse place on Earth.</p>            
-                    </div>
-                </div>
-
-                <div className="highlights-content">
-                    <div className="highlights-image-wrapper">
-                        <img className="highlights-image" src={Topiary} alt="Meaningful Text"/>
-                    </div>
-                    <div className="highlights-text">
-                        <h3>Visit the world-famous Kew Gardens</h3>  
-                        <p>Experience our beautiful botanic gardens and the most biodiverse place on Earth.</p>            
+                        <h3>{props.title}</h3>  
+                        <p>{props.description}</p>            
                     </div>
                 </div>
             </div>
